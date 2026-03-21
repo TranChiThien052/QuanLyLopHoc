37e4ffbe61c8efabfb62df8f27420497ef51879b
TueTran <bigbang159vn@gmail.com>
2026-03-21 11:10:51 +0700

    test2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Dự án Quản Lý Lớp Học - STU</h1>
-      <h3>Nhóm 1 - Thầy Bằng</h3>
+      <h1>Dự án Quản Lý Lớp Học - STU - Nhóm 1 - Thầy Bằng</h1>
     </div>
   );
 }
