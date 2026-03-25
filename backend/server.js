afe4f4f9054fef378d10123424fb4b4e6d89876e
nminhtam426425 <nminhtam426425@gmail.com>
2026-03-25 10:19:42 +0700

    test thu vien bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Xin chào từ Node.js!");
+});
+
+app.use(express.json());
+
+app.post("/createAccount", async (req,res) => {
+  const hasdBcrypt = await bcrypt.hash(req.body.pass,10);
+  res.status(200).json({code:200,data:hasdBcrypt});
+});
+
+app.post('/loginTest', async (req, res) => {
+  const myPass = "$2b$10$0fXGKGntaWe/BC/UZwefaesaeBmgU5SqQnaNbePOzft/qUJJJwS2q";
+  const isMatch = await bcrypt.compare(req.body.pass, myPass);
+  if(isMatch){
+    res.status(200).json(
+      {
+        code:200,
+        data:{
+          token:""
+        }
+      });
+  }
+  else
+    res.status(401).json(
+      {
+        code:401,
+        message:"Đăng nhập thất bại !"
+      }
+    );
 });
 
 app.listen(3000, () => {
