4c51351ed55f96beba709a4c4e00b891a76b9960
TueTran <bigbang159vn@gmail.com>
2026-04-10 23:43:55 +0700

    fe login, QLLH, profile. Cua teacher

--- frontend/src/pages/teacher/Profile.jsx
+++ frontend/src/pages/teacher/Profile.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import "./TeacherProfile.css";
 
-function TeacherProfile() {
+export default function TeacherProfile() {
   return (
-    <div>
-      <h3>Thông tin cá nhân (Giảng viên)</h3>
-      <p>Họ tên: TS. Nguyễn Văn B</p>
-      <p>Mã GV: GV001</p>
-      <p>Khoa: Công nghệ thông tin</p>
+    <div className="profile-container">
+      
+      <h2 className="profile-title">Thông tin cá nhân</h2>
+
+      <div className="profile-content">
+
+        {/* LEFT: AVATAR */}
+        <div className="profile-left">
+          <div className="avatar-box">
+            <img src="https://placehold.co/285x273" alt="avatar" />
+          </div>
+
+          <button className="btn-edit-avatar">
+            Chỉnh ảnh
+          </button>
+        </div>
+
+        {/* RIGHT: INFO */}
+        <div className="profile-right">
+          
+          <div className="form-row">
+            <label>Mã giáo viên</label>
+            <input type="text" value="GV021" readOnly />
+          </div>
+
+          <div className="form-row">
+            <label>Họ tên</label>
+            <input type="text" value="Nguyễn Văn A" />
+          </div>
+
+          <div className="form-row">
+            <label>Ngày sinh</label>
+            <input type="date" />
+          </div>
+
+          <div className="form-row">
+            <label>Giới tính</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-row">
+            <label>Địa chỉ</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-row">
+            <label>Email</label>
+            <input type="email" />
+          </div>
+
+          <div className="form-row">
+            <label>Số điện thoại</label>
+            <input type="text" />
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
-
-export default TeacherProfile;
