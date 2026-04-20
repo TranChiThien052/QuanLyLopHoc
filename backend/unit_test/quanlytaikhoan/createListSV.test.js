056f0b8b6abfd9f3fcb0035e689061f8c3ba6d36
nminhtam426425 <nminhtam426425@gmail.com>
2026-04-20 21:51:44 +0700

    chuyen nhanh

--- backend/unit_test/quanlytaikhoan/createListSV.test.js
+++ backend/unit_test/quanlytaikhoan/createListSV.test.js
@@ -4,24 +4,86 @@ const  {sequelize} = require('../../models');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 
-describe('Kiểm thử API Tạo DSSV', () => {
+describe('Chưa đăng nhập', () => {
+
+    it('Tạo danh sách bại', async () => {
+        const filePath = path.join(__dirname, 'fileTest/ds-test.xlsx');
+        const response = await request(app)
+        .post('/students/bulk')
+        .set('Authorization', `Bearer `)
+    
+        expect(response.statusCode).toBe(401);
+       
+    });
+});
+
+describe('Đã đăng nhập, không có quyền', () => {
+    let testToken = "";
+
+    beforeAll(() => {
+        const payload = { id: 'GV_TH00001',role:'teacher' };
+        const secret = process.env.JWT_SECRET || 'your_secret_key';
+        testToken = jwt.sign(payload, secret, { expiresIn: '1h' });
+    });
+
+    it('Tạo danh sách thát bại', async () => {
+        const filePath = path.join(__dirname, 'fileTest/ds-test.xlsx');
+        const response = await request(app)
+        .post('/students/bulk')
+        .set('Authorization', `Bearer ${testToken}`)
+    
+        expect(response.statusCode).toBe(403);
+       
+    });
+});
+
+describe('Đã đăng nhập', () => {
     let testToken = "";
 
     beforeAll(() => {
         const payload = { id: 'admin00001',role:'admin' };
         const secret = process.env.JWT_SECRET || 'your_secret_key';
-        //testToken = jwt.sign(payload, secret, { expiresIn: '1h' });
+        testToken = jwt.sign(payload, secret, { expiresIn: '1h' });
     });
 
-    it('Tạo danh sách fail', async () => {
+    it('Tạo danh sách thành công', async () => {
         const filePath = path.join(__dirname, 'fileTest/ds-test.xlsx');
         const response = await request(app)
         .post('/students/bulk')
         .set('Authorization', `Bearer ${testToken}`)
-        .attach('excelFile', filePath)
+        .attach('excelFile',filePath)
     
-        expect(response.statusCode).toBe(403);
-       
+        expect(response.statusCode).toBe(200);
+    });
+
+    it('Tạo danh sách thất bại, sai định dạng file', async () => {
+        const filePath = path.join(__dirname, 'fileTest/ds-test.txt');
+        const response = await request(app)
+        .post('/students/bulk')
+        .set('Authorization', `Bearer ${testToken}`)
+        .attach('excelFile',filePath)
+    
+        expect(response.statusCode).toBe(400);
+    });
+
+    it('Tạo danh sách thất bại, thiếu cột quan trọng', async () => {
+        const filePath = path.join(__dirname, 'fileTest/ds-test-thieu-ma.xlsx');
+        const response = await request(app)
+        .post('/students/bulk')
+        .set('Authorization', `Bearer ${testToken}`)
+        .attach('excelFile',filePath)
+    
+        expect(response.statusCode).toBe(400);
+    });
+
+    it('Tạo danh sách thất bại, định dạng dữ liệu không đúng', async () => {
+        const filePath = path.join(__dirname, 'fileTest/ds-test-sai-dinh-dang.xlsx');
+        const response = await request(app)
+        .post('/students/bulk')
+        .set('Authorization', `Bearer ${testToken}`)
+        .attach('excelFile',filePath)
+    
+        expect(response.statusCode).toBe(500);
     });
 });
 
